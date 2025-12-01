d56b8b28f66b272f23e87233acabb61603813e06
egekurt123 <egekurt14@gmail.com>
2025-12-01 10:14:23 +0100

    ruff reformat

--- cdm/prompts/cdm.py
+++ cdm/prompts/cdm.py
@@ -5,7 +5,9 @@ from jinja2 import Environment, FileSystemLoader, Template
 TEMPLATE_DIR = Path(__file__).parent
 jinja_env = Environment(loader=FileSystemLoader(searchpath=TEMPLATE_DIR))
 
-format_instructions_template: Template = jinja_env.get_template("format_instructions/diagnosis_treatment.j2")
+format_instructions_template: Template = jinja_env.get_template(
+    "format_instructions/diagnosis_treatment.j2"
+)
 format_instructions = format_instructions_template.render()
 
 system_prompt_template: Template = jinja_env.get_template("tool_calling/system.j2")
--- cdm/prompts/full_info.py
+++ cdm/prompts/full_info.py
@@ -5,7 +5,9 @@ from jinja2 import Environment, FileSystemLoader, Template
 TEMPLATE_DIR = Path(__file__).parent
 jinja_env = Environment(loader=FileSystemLoader(searchpath=TEMPLATE_DIR))
 
-format_instructions_template: Template = jinja_env.get_template("format_instructions/only_diagnosis.j2")
+format_instructions_template: Template = jinja_env.get_template(
+    "format_instructions/only_diagnosis.j2"
+)
 format_instructions = format_instructions_template.render()
 
 system_prompt_template: Template = jinja_env.get_template("full_info/system.j2")
